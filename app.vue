5357a04c41df76777e55492cc2b209e2f83154ab
yangliu <a634691481@gmail.com>
2026-07-29 11:35:21 +0800

    fix: PWA - 注入manifest + 添加PNG图标

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <ParticleBg />
+  <VitePwaManifest />
   <NuxtPage />
   <Toast />
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,7 @@ export default defineNuxtConfig({
 
   pwa: {
     registerType: 'autoUpdate',
+    registerWebManifestInRouteRules: true,
     workbox: {
       globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2,wav,mp3}'],
       runtimeCaching: [
@@ -63,6 +64,17 @@ export default defineNuxtConfig({
           src: '/favicon.svg',
           sizes: 'any',
           type: 'image/svg+xml',
+          purpose: 'any maskable',
+        },
+        {
+          src: '/icons/icon-192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/icon-512.png',
+          sizes: '512x512',
+          type: 'image/png',
           purpose: 'any maskable',
         },
       ],
